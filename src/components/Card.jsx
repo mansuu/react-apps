35a4a12219e97bc02b572539a472351e75ad8aca
Himanshu Srivastava <manasshrivu@gmail.com>
2024-06-25 18:32:17 +0530

    sliding movie cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,6 +10,7 @@ import { BsCheck } from "react-icons/bs";
 
 export default function Card({ cardObject }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
   const navigate = useNavigate();
   return (
     <Container
@@ -47,23 +48,121 @@ export default function Card({ cardObject }) {
               <source src={video} type="video/mp4" />
             </video>
           </div>
+          <div className="info-container flex column">
+            <h3
+              className="name"
+              onClick={() => {
+                navigate("/player");
+              }}
+            >
+              {cardObject?.name}
+            </h3>
+            <div className="icons flex j-between">
+              <div className="controls flex">
+                <IoPlayCircleSharp
+                  onClick={() => {
+                    navigate("/player");
+                  }}
+                />
+                <RiThumbUpFill title="Like" />
+                <RiThumbDownFill title="Dislike" />
+                {isLiked ? (
+                  <BsCheck title="Remove from list" />
+                ) : (
+                  <AiOutlinePlus title="Add to my list" />
+                )}
+              </div>
+              <div className="info">
+                <BiChevronDown title="More info" />
+              </div>
+            </div>
+            <div className="genres flex">
+              <ul className="flex">
+                {cardObject.genres.map((genre) => {
+                  return <li key={genre}>{genre}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
         </div>
       ) : null}
-      <div className="info-container flex column">
-        <h3
-          className="name"
-          onClick={() => {
-            navigate("/player");
-          }}
-        >
-          {cardObject?.name}
-        </h3>
-        <div className="icons flex j-between">
-          <div className="controls flex"></div>
-        </div>
-      </div>
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  height: 100%;
+  width: 230px;
+  max-width: 230px;
+  position: relative;
+  cursor: pointer;
+  img {
+    height: 100%;
+    width: 100%;
+    border-radius: 0.2rem;
+    z-index: 10;
+  }
+  .hover {
+    z-index: 90;
+    height: max-content;
+    width: 20rem;
+    position: absolute;
+    top: -18vh;
+    left: 0;
+    border-radius: 0.3rem;
+    box-shadow: rgba(0, 0, 0, 0.75) 0px 10px 10px;
+    background-color: #181818;
+    transition: 0.3s ease-in-out;
+    .image-video-container {
+      height: 140px;
+      position: relative;
+      img {
+        width: 100%;
+        height: 140px;
+        object-fit: cover;
+        border-radius: 0.3rem;
+        top: 0;
+        position: absolute;
+        z-index: 4;
+      }
+      video {
+        width: 100%;
+        height: 140px;
+        object-fit: cover;
+        border-radius: 0.3rem;
+        top: 0;
+        position: absolute;
+        z-index: 5;
+      }
+    }
+    .info-container {
+      padding: 1rem;
+      gap: 0.5rem;
+    }
+    .icons {
+      .controls {
+        gap: 1rem;
+        display: flex;
+      }
+      svg {
+        font-size: 2rem;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: #b8b8b8;
+        }
+      }
+    }
+    .genres {
+      ul {
+        gap: 1rem;
+        li {
+          padding-right: 0.7rem;
+          &:first-of-type {
+            list-style-type: none;
+          }
+        }
+      }
+    }
+  }
+`;
--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,12 +1,104 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Card from "./Card";
+import styled from "styled-components";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 export default function CardSlider({ title, data }) {
+  const [showControls, setShowControls] = useState(false);
+  const listRef = useRef();
+  const [sliderPosition, setSliderPosition] = useState(0);
+  const onSliderAction = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 70;
+    if (direction === "left" && sliderPosition > 0) {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      setSliderPosition(sliderPosition - 1);
+    }
+    if (direction === "right" && sliderPosition < 4) {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      setSliderPosition(sliderPosition + 1);
+    }
+  };
   return (
-    <div key={title}>
-      {data?.map((el) => {
-        return <Card cardObject={el} key={el?.id} />;
-      })}
-    </div>
+    <Container
+      key={title}
+      className="flex column"
+      onMouseEnter={() => {
+        setShowControls(true);
+      }}
+      onMouseLeave={() => {
+        setShowControls(false);
+      }}
+    >
+      <h1>{title}</h1>
+      <div className="wrapper">
+        <div
+          className={`slider-action left ${
+            !showControls ? "none" : ""
+          } flex a-center j-center`}
+        >
+          <AiOutlineLeft
+            onClick={() => {
+              onSliderAction("left");
+            }}
+          />
+        </div>
+        <div className="flex slider" ref={listRef}>
+          {data?.map((el) => {
+            return <Card cardObject={el} key={el?.id} />;
+          })}
+        </div>
+        <div
+          className={`slider-action right ${
+            !showControls ? "none" : ""
+          } flex a-center j-center`}
+        >
+          <AiOutlineRight
+            onClick={() => {
+              onSliderAction("right");
+            }}
+          />
+        </div>
+      </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  gap: 1rem;
+  position: relative;
+  padding: 2rem 0;
+  h1 {
+    margin-left: 50px;
+  }
+  .wrapper {
+    .slider {
+      gap: 1rem;
+      width: max-content;
+      transform: translate(0px);
+      transition: 0.3s ease-in-out;
+      margin-left: 50px;
+    }
+    .slider-action {
+      position: absolute;
+      height: 100%;
+      top: 0;
+      width: 50px;
+      bottom: 0;
+      //margin-left: 50px;
+      transition: 0.3s ease-in-out;
+      z-index: 99;
+      svg {
+        font-size: 2rem;
+      }
+    }
+    .none {
+      display: none;
+    }
+    .left {
+      left: 0;
+    }
+    .right {
+      right: 0;
+    }
+  }
+`;
